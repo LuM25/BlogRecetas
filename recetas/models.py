46e50bfc69294775964e145b8cd631a7498d1ea1
Lucia.Molina <luciamacarenamolina@gmail.com>
2022-07-04 17:40:07 -0300

    last changes

--- recetas/models.py
+++ recetas/models.py
@@ -5,14 +5,15 @@ from django.db import models
 class Receta(models.Model):
     nombre = models.CharField(max_length = 30)
     tipo = models.CharField(max_length = 30)
+    porciones = models.FloatField(default = 0)
     ing_y_cant = models.CharField(max_length = 30)
     procedimiento = models.CharField(max_length = 30)
     is_active = models.BooleanField(default = True)
-    image = models.ImageField(upload_to = 'receta', blank = True , null = True)
+    image = models.ImageField(upload_to = 'recetas', blank = True , null = True)
 
     class Meta:
-        verbose_name = 'producto'
-        verbose_name_plural = 'productos'
+        verbose_name = 'receta'
+        verbose_name_plural = 'recetas'
 
     def __str__(self):
         return self.nombre
